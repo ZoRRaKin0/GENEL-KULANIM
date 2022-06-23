74dc881e6fdba23ea01906a7acdbc6855ba0e237
ZoRRaKin <105127547+ZoRRaKin0@users.noreply.github.com>
2022-06-23 21:28:10 +0300

    Add files via upload

---
+++ genel-kullanım.py
@@ -0,0 +1,35 @@
+# coded by ZoRRaKin
+import os
+import time
+import webbrowser
+os.system('pkg install cmatrix')
+isim = str(input("BİLGİLERİ ARAŞTIRILACAK İSİM'İ TAM SEKİLDE YAZINIZ : "))
+sosyal_medya = str(input("SOSYAL MEDYA KULLANICI ADINI GİRİNİZ :"))
+telefon_numarası = str(input("TELEFON NUMARASI GİRİNİZ :"))
+time.sleep(5)
+print("devam etmek icin benden sifreyi alınız")
+time.sleep(2.1)
+sifre = str(input("SİFREYİ GİRİNİZ :"))
+if sifre =="ZoRRaKin1905" or sifre == "ZoRRaKina1b2c3d4e5":
+    sms_sayısı = int(input("kac tane sms atalım   (max = 100.000) :"))
+    if sms_sayısı <=100.000:
+        print("ŞİFRE DOGRULANDII HOŞGELDİNİZ")
+        time.sleep(3)
+        print("ATTACK BASLATILDI...")
+        time.sleep(1.5)
+        print("LÜTFEN BEKLEYİNİZ...")
+        time.sleep(459.7)
+        print("SONUCLAR TOPLANIYOR")
+        time.sleep(2.1)
+        prrint("ATTACK BİTTİ... SONUCLARI @acheron0 kullanıcı adlı kisiden alabilirsiniz")
+        time.sleep(15)
+        os.system('cmatrix')
+    else:
+        time.sleep(4.2)
+        print("GİRDİGİNİZ SAYI UYGUN DEGIL.")
+        os.system('cmatrix')
+else:
+    time.sleep(5)
+    print("HATALI SİFRE GİRDİNİZ LÜTFEN BENDEN SİFRE ALINIZ")
+    os.system('cmatrix')
+    
